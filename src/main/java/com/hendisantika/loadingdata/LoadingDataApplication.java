18b4ad484a84143b41aa7e93ed6567d6410a76c8
Hendi Santika <hendisantika@gmail.com>
2018-04-22 06:00:12 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/loadingdata/LoadingDataApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.loadingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadingDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadingDataApplication.class, args);
+    }
+}
