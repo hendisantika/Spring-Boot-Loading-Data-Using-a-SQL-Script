91f9e7d5843f554636d35e295f09a644230abb21
Hendi Santika <hendisantika@gmail.com>
2018-10-11 04:25:07 +0700

    #12 - Modify sql script

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,9 +1,5 @@
-insert into Person
-(last_name, first_name)
-values
-('Uzumaki', 'Naruto');
+insert into Person (id, last_name, first_name)
+values (1, 'Uzumaki', 'Naruto');
 
-insert into Person
-(last_name, first_name)
-values
-('Uchiha', 'Sasuke');
+insert into Person (id, last_name, first_name)
+values (2, 'Uchiha', 'Sasuke');
